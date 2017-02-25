b0464f6b4aefd85ef3af010a85f9b62c2d9f9090
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2017-02-25 14:51:03 -0500

    colby::optional et al.
    Polyfill for C++17 std::optional.

---
+++ include/colby/optional.hpp
@@ -0,0 +1,18 @@
+/**
+ *	\file
+ */
+
+#pragma once
+
+#include <experimental/optional>
+
+namespace colby {
+
+using std::experimental::bad_optional_access;
+using std::experimental::in_place;
+using std::experimental::in_place_t;
+using std::experimental::nullopt;
+using std::experimental::nullopt_t;
+using std::experimental::optional;
+
+}
