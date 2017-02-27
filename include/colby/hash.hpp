151c83a3064dbd6ccd34beb9217bdd2022dcfcb5
Jordan Heemskerk <jordanh@shaw.ca>
2017-02-26 21:09:48 -0800

    std::hash for cv::Vec<T,N>

--- include/colby/hash.hpp
+++ include/colby/hash.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <boost/functional/hash.hpp>
+#include <opencv2/core/matx.hpp>
 #include <opencv2/core/types.hpp>
 #include <cstddef>
 #include <functional>
@@ -23,5 +24,18 @@ public:
 		return retr;
 	}
 };
+
+template <typename T, int N>
+class hash<cv::Vec<T,N>> {
+public:
+	using argument_type = cv::Vec<T,N>;
+	using result_type = std::size_t;
+	result_type operator () (const argument_type & vec) const noexcept {
+		std::size_t retr = 0;
+		for (int i = 0; i < N; ++i) boost::hash_combine(retr, vec[i]);
+		return retr;
+	}
+};
+
 
 }
